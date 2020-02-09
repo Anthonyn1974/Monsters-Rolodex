836d5c447bf3a1543a9eb3cd24b517e176080c13
Anthony Nicholls <Anthony.Nicholls@gmail.com>
2020-02-09 19:34:36 +0000

    Updates

---
+++ precache-manifest.33a22feb4fa4e896bb322409995e7c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e809f7c5645fe4b0ca95538d8d01ea16",
+    "url": "AnthonyN1974.github.io/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "c0dd5eb94934ced542ff",
+    "url": "AnthonyN1974.github.io/Monsters-Rolodex/static/css/main.9bde0972.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "AnthonyN1974.github.io/Monsters-Rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "AnthonyN1974.github.io/Monsters-Rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0dd5eb94934ced542ff",
+    "url": "AnthonyN1974.github.io/Monsters-Rolodex/static/js/main.b62ac52a.chunk.js"
+  },
+  {
+    "revision": "62f9dbb8c12a2ef68130",
+    "url": "AnthonyN1974.github.io/Monsters-Rolodex/static/js/runtime-main.79b3e9b9.js"
+  }
+]);
